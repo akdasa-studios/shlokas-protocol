4fba5a8fbc7185a907dfdd095f6a7b80efd87e3b
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-05 14:59:48 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: './.temp/coverage',
+  coverageProvider: 'v8',
+  testMatch: [
+    '<rootDir>/tests/**/*.spec.ts'
+  ],
+  moduleNameMapper: {
+    '^@lib/(.*)': '<rootDir>/lib/$1',
+    '^@tests/(.*)': '<rootDir>/tests/$1',
+  },
+  setupFilesAfterEnv: [
+    'jest-extended/all'
+  ]
+}
